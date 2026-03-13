309b192a55b33801d75814b4979f5a4f26ec0535
Gabriel <gabrielsb1090@gmail.com>
2026-03-13 10:55:40 -0300

    Removendo component1

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,9 @@
-import Component1 from "./Components/Component1/Component1"
-import "../src/Components/Component1/Component1.css"
-
-
 import Form from "./Components/Form/Form"
-
 
 function App() {
 
-  const pessoas = [
-    {nome: "Gabriel", idade: 23, profissao: "Programador", cor: "red"},
-    {nome: "Julia", idade: 20, profissao: "Advogada", cor: "blue"},
-    {nome: "Matheus", idade: 30, profissao: "Mecanico", cor: "green"},
-    {nome: "Juliana", idade: 29, profissao: "Gerente", cor: "orange"},
-    {nome: "João", idade: 33, profissao: "Padeiro", cor: "gray"}
-  ]
-
   return (
     <>
-    <div id="container">
-      {pessoas.map((pessoa, index) => (
-        <Component1 
-          key={index}
-          nome={pessoa.nome}
-          idade={pessoa.idade}
-          profissao={pessoa.profissao}
-          cor={pessoa.cor}
-        />
-      ))}
-    </div>
     <Form />
     </>
   )
