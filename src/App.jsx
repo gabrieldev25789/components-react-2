fbd69ab34e16fc32a4fac6bdfc2e20fb682207a2
Gabriel <gabrielsb1090@gmail.com>
2026-03-13 07:18:57 -0300

    Usando component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,28 @@ import Component1 from "./Components/Component1/Component1"
 
 function App() {
 
+  const pessoas = [
+    {nome: "Gabriel", idade: 23, profissao: "Programador", cor: "red"},
+    {nome: "Julia", idade: 20, profissao: "Advogada", cor: "blue"},
+    {nome: "Matheus", idade: 30, profissao: "Mecanico", cor: "green"},
+    {nome: "Juliana", idade: 29, profissao: "Gerente", cor: "orange"},
+    {nome: "João", idade: 33, profissao: "Padeiro", cor: "gray"}
+  ]
+
   return (
     <>
-      <h1></h1>
-      <Component1 nome={"Gabriel"} />
+      {pessoas.map((pessoa, index) => (
+        <Component1 
+          key={index}
+          nome={pessoa.nome}
+          idade={pessoa.idade}
+          profissao={pessoa.profissao}
+          cor={pessoa.cor}
+        />
+      ))}
     </>
   )
 }
+
 
 export default App
