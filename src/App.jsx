8819db90c88552878692ee65b74fdfb2e0a1ae2a
Gabriel <gabrielsb1090@gmail.com>
2026-03-13 06:05:05 -0300

    Usando primeiro component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import Component1 from "./Components/Component1/Component1"
+
 
 function App() {
 
   return (
     <>
-      <h1>HELLOOO NIKKKKKAAASSSS</h1>
+      <h1></h1>
+      <Component1 nome={"Gabriel"} />
     </>
   )
 }
