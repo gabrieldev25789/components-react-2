06169b7c8356b061a8dcf9873230ccf76c3dc85d
Gabriel <gabrielsb1090@gmail.com>
2026-03-18 11:46:44 -0300

    Criando btns de exluir e editar

--- src/Components/Form/Infos/Infos.jsx
+++ src/Components/Form/Infos/Infos.jsx
@@ -7,6 +7,11 @@ function Infos({ nome, idade, profissao}) {
                 <li>Nome: {nome}</li>
                 <li>Idade: {idade}</li>
                 <li>Profissão: {profissao}</li>
+            <li id="btns">
+                <button id="editar">Editar</button>
+                <button id="excluir">Excluir</button>
+            </li>
+
             </ul>
     </div>
   )
